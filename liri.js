90ae55f499edd675f4404774520b766e20a9b8d7
Lindsey Bowen <lindsbow102@yahoo.com>
2018-04-14 16:59:55 -0700

    got omdb to work

--- liri.js
+++ liri.js
@@ -10,76 +10,77 @@ var client = new Twitter(keys.twitter);
 var Spotify = require('node-spotify-api');
 var spotify1 = new Spotify(keys.spotify);
 
-var arg1 = process.argv[2];
-var arg2 = process.argv[3];
+var nodeArgs = process.argv;
 
 
-function myTweets() {
-    //Display last 20 Tweets
-    var params = {
-        screen_name: 'Lindsbow102',
-        count: 20
-    };
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-            for (var i = 0; i < tweets.length; i++) {
-                var date = tweets[i].created_at;
-                console.log("@Lindsbow102: " + tweets[i].text + " Created At: " + date.substring(0, 19));
-                console.log("-----------------------");
-            }
-        } else {
-            console.log('Error occurred');
-        }
-    });
-}
+//function myTweets() {
 
-var movieThis = function (movieQuery) {
+//screen_name: 'Lindsbow102',
+//count: 20
+// };
+//client.get('statuses/user_timeline', params, function (error, tweets, response) {
+//if (!error) {
+//for (var i = 0; i < tweets.length; i++) {
+// var date = tweets[i].created_at;
+// console.log("@Lindsbow102: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+// console.log("-----------------------");
+//  }
+//  } else {
+//      console.log('Error occurred');
+//  }
+//   });
+//}
+
+var movieThis = function (movieName) {
     // Load request npm module
     var request = require("request");
-
-    // if query that is passed in is undefined, Mr. Nobody becomes the default
-    if (movieQuery === undefined) {
-        movieQuery = "mr nobody";
+    // Create an empty variable for holding the movie name
+    var movieName = "";
+    // Loop through all the words in the node argument
+    // And do a little for-loop magic to handle the inclusion of "+"s
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+            movieName = movieName + "+" + nodeArgs[i];
+        } else {
+            movieName += nodeArgs[i];
+        }
     }
-
-    // HTTP GET request
-    request("http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&r=json", function (error, response, body) {
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+    if (movieName === undefined) {
+        movieName = "mr nobody";
+    }
+    request(queryUrl, function (error, response, body) {
+    // If the request is successful
         if (!error && response.statusCode === 200) {
-            console.log("* Title of the movie:         " + JSON.parse(body).Title);
-            console.log("* Year the movie came out:    " + JSON.parse(body).Year);
-            console.log("* IMDB Rating of the movie:   " + JSON.parse(body).imdbRating);
-            console.log("* Country produced:           " + JSON.parse(body).Country);
-            console.log("* Language of the movie:      " + JSON.parse(body).Language);
-            console.log("* Plot of the movie:          " + JSON.parse(body).Plot);
-            console.log("* Actors in the movie:        " + JSON.parse(body).Actors);
-
-            // For loop parses through Ratings object to see if there is a RT rating
-            // 	--> and if there is, it will print it
-            for (var i = 0; i < JSON.parse(body).Ratings.length; i++) {
-                if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
-                    console.log("* Rotten Tomatoes Rating:     " + JSON.parse(body).Ratings[i].Value);
-                    if (JSON.parse(body).Ratings[i].Website !== undefined) {
-                        console.log("* Rotten Tomatoes URL:        " + JSON.parse(body).Ratings[i].Website);
-                    }
-                }
-            }
+            // Display movie info in terminal
+            console.log("* Title of the movie: " + JSON.parse(body).Title);
+            console.log("* Year the movie came out: " + JSON.parse(body).Year);
+            console.log("* IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
+            console.log("* Country produced: " + JSON.parse(body).Country);
+            console.log("* Language of the movie: " + JSON.parse(body).Language);
+            console.log("* Plot of the movie: " + JSON.parse(body).Plot);
+            console.log("* Actors in the movie: " + JSON.parse(body).Actors);
         }
     });
 }
 
-switch (arg1) {
-    case "my-tweets":
-        myTweets();
-        break;
+switch (nodeArgs[2]) {
+    //case "my-tweets":
+    // myTweets();
+    // break;
 
     case "movie-this":
-        movieThis(arg2);
+        movieThis();
         break;
 
-    default:
-        console.log("{Please enter a command: my-tweets, spotify-this-song, //movie-this, do-what-it-says}");
-        break;
+    //default:
+    // console.log("{Please enter a command: my-tweets, spotify-this-song, //movie-this, do-what-it-says}");
+    // break;
 
 }
+//}
 
 
