d1fde99ecd5520e66a7659d8d0a2c13631ddae61
Lindsey Bowen <lindsbow102@yahoo.com>
2018-04-15 14:22:34 -0700

    still trying

--- liri.js
+++ liri.js
@@ -1,35 +1,36 @@
 require("dotenv").config();
 
 const keys = require('./keys');
-
-fs = require('fs');
+var fs = require('fs');
 
 var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
+var params = {
+    screen_name: 'Lindsbow102',
+    count: 20
+};
 
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-
 var nodeArgs = process.argv;
 
-
-//function myTweets() {
-
-//screen_name: 'Lindsbow102',
-//count: 20
-// };
-//client.get('statuses/user_timeline', params, function (error, tweets, response) {
-//if (!error) {
-//for (var i = 0; i < tweets.length; i++) {
-// var date = tweets[i].created_at;
-// console.log("@Lindsbow102: " + tweets[i].text + " Created At: " + date.substring(0, 19));
-// console.log("-----------------------");
-//  }
-//  } else {
-//      console.log('Error occurred');
-//  }
-//   });
-//}
+//Getting terminal to display last 20 tweets
+var myTweets = function() {
+	
+	client.get('search/tweets', params, function(error, tweets, response) {
+	  if (!error) {
+        //testing to see if this even works
+        console.log(JSON.stringify(response));
+	  	// Loops through tweets and prints out tweet text and creation date.
+	  	for (var i = 0; i < tweets.statuses.length; i++) {
+	  		var tweetText = tweets.statuses[i].text;
+	  		console.log("Tweet Text: " + tweetText);
+	  		var tweetCreationDate = tweets.statuses[i].created_at;
+	  		console.log("Tweet Creation Date: " + tweetCreationDate);
+	  	}
+	  } 
+	})
+}
 
 var movieThis = function (movieName) {
     // Load request npm module
@@ -68,7 +69,7 @@ var movieThis = function (movieName) {
 }
 
 var spotifyThisSong = function () {
-
+    //create empty variable to hold song name
     var songName = "";
 
     if (songName === undefined) {
@@ -88,22 +89,44 @@ var spotifyThisSong = function () {
         }
         for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
             if (i === 0) {
-                console.log("Artist(s): " + data.tracks.items[0].artists[i].name);
+                console.log("* Artist(s): " + data.tracks.items[0].artists[i].name);
             } else {
                 console.log("  " + data.tracks.items[0].artists[i].name);
             }
         }
-        console.log("Song: " + data.tracks.items[0].name);
-        console.log("Preview Link: " + data.tracks.items[0].preview_url);
-        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("* Song: " + data.tracks.items[0].name);
+        console.log("* Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("* Album: " + data.tracks.items[0].album.name);
         //console.log(JSON.stringify(data, null, 2)); 
     })
 }
 
+function doWhatItSays() {
+
+	fs.readFile("random.txt", "utf8", function(err, data) {
+		if (err) {
+			logOutput.error(err);
+		} else {
+
+			// Creates array with data.
+			var randomArray = data.split(",");
+
+			// Sets action to first item in array.
+			action = randomArray[0];
+
+			// Sets optional third argument to second item in array.
+			argument = randomArray[1];
+
+			// Calls main controller to do something based on action and argument.
+			spotifyThisSong(action, argument);
+		}
+	});
+}
+
 switch (nodeArgs[2]) {
-    //case "my-tweets":
-    // myTweets();
-    // break;
+    case "my-tweets":
+        myTweets();
+        break;
 
     case "movie-this":
         movieThis();
@@ -113,11 +136,15 @@ switch (nodeArgs[2]) {
         spotifyThisSong();
         break;
 
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+
     default:
      console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
      break;
 
 }
-//}
+
 
 
